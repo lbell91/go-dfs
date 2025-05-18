de30ca9dc50c60a45e9f19a3a44b9ee00ea06587
Liam <liam.bellc@gmail.com>
2025-05-17 21:36:59 -0400

    quick test for connection

--- main.go
+++ main.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
+
 	"github.com/lbell91/go-dfs/p2p"
 )
 
 func main() {
 	transport := p2p.BuildTcpTransport(":4000")
 
+	if err := transport.ListenAndAccept(); err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println("Good to go for now")
+
+	select {
+
+	}
 }
